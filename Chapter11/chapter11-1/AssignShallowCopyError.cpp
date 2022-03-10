e6892c2c59d07bd4a5adbf25ddb22179a825c373
oingggum <ezzeezz@naver.com>
2022-03-10 23:37:26 +0900

    ch11

---
+++ Chapter11/chapter11-1/AssignShallowCopyError.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Person
+{
+private:
+	char* name;
+	int age;
+public:
+	Person(const char* myname, int myage)
+	{
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy_s(name, len, myname);
+		age = myage;
+	}
+	Person& operator=(const Person& copy)
+	{
+		delete[]name;
+		int len = strlen(copy.name) + 1;
+		name = new char[len];
+		strcpy_s(name, len, copy.name);
+		age = copy.age;
+
+		return *this;
+	}
+	void ShowPersonInfo() const
+	{
+		cout << "이름 : " << name << endl;
+		cout << "나이 : " << age << endl;
+	}
+	~Person()
+	{
+		delete[]name;
+		cout << "called destructor!" << endl;
+	}
+};
+
+int main()
+{
+	Person man1("Lee dong woo", 29);
+	Person man2("Yoon ji yul", 22);
+	man2 = man1;
+
+	man1.ShowPersonInfo();
+	man2.ShowPersonInfo();
+	return 0;
+}
